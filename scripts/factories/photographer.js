d17fafe1922dfd5bd2875b3213d84ba87169d6c3
Foxest57 <108059438+FOXEST57@users.noreply.github.com>
2022-09-16 19:23:46 +0200

    sauvegarde

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,22 @@
 export default photographerFactory;
 
 
+
+
 function photographerFactory(data) {
     const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement('article');
+        const article = document.createElement('a');
+        article.setAttribute('href', 'photographer.html?id=' + 'test');
+        console.log(photographers);
         const img = document.createElement('img');
         img.setAttribute("src", picture)
         const h2 = document.createElement('h2');
         h2.textContent = name;
+
         const h3 = document.createElement('h3');
         h3.textContent = city + ',  ' + country;
         const h4 = document.createElement('h4');
@@ -28,3 +33,31 @@ function photographerFactory(data) {
     }
     return { name, picture, city, country, tagline, price, getUserCardDOM }
 }
+
+// function photographerFactory(data) {
+//     const { name, portrait, city, country, tagline, price } = data;
+
+//     const picture = `assets/photographers/${portrait}`;
+
+//     function getUserCardDOM() {
+//         // const link = document.createElement('a');
+//         // const img = document.createElement('img');
+//         // link.setAttribute("src", 'photographer.html')
+//         // const h2 = document.createElement('h2');
+//         // h2.textContent = name;
+//         // const h3 = document.createElement('h3');
+//         // h3.textContent = city + ',  ' + country;
+//         // const h4 = document.createElement('h4');
+//         // h4.textContent = tagline;
+//         // const p = document.createElement('p');
+//         // p.textContent = price + ' €/jour';
+//         // link.appendChild(img);
+//         // link.appendChild(h2);
+//         // link.appendChild(h3);
+//         // link.appendChild(h4)
+//         // link.appendChild(p);
+
+//         // return (link);
+//     }
+//     return { name, picture, city, country, tagline, price, getUserCardDOM }
+// }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,6 +1,8 @@
 import getPhotographers from '/data/api.js'
 import photographerFactory from '/scripts/factories/photographer.js'
 
+export default displayData;
+
 async function displayData(photographers) {
     const photographersSection = document.querySelector(".photographer_section");
 
@@ -17,5 +19,25 @@ async function init() {
     displayData(photographers);
 
 };
+// async function init() {
+//     //     // Récupère les datas des photographes
+//     const { photographers } = await getPhotographers();
+
+//     photographers.forEach((photographer) => {
+//         const link = document.createElement('a');
+//         link.setAttribute('href', 'photographer.html')
+
+//         link.innerHTML = `
+//             <img src="assets/photographers/${photographer.portrait}"/>
+//             <h2>${photographer.name}</h2>
+//             <h3>${photographer.city + ' ' + photographer.country}</h3>
+//             <h3 class="test">${photographer.tagline}</h3>
+//             <p class="prix">${photographer.price + ' €/jour'}</h3>
+
+//             `
+//         document.querySelector(".photographer_section").appendChild(link)
+//     });
+// };
+
 
 init();
