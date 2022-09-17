5d170a098ef99257340314af4b840115c93bb127
Foxest57 <108059438+FOXEST57@users.noreply.github.com>
2022-09-17 12:29:22 +0200

    sauvegarde

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -4,14 +4,14 @@ export default photographerFactory;
 
 
 function photographerFactory(data) {
-    const { name, portrait, city, country, tagline, price } = data;
+    const { name, portrait, city, country, tagline, price, id } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement('a');
-        article.setAttribute('href', 'photographer.html?id=' + 'test');
-        console.log(photographers);
+        article.setAttribute('href', 'photographer.html?id=' + id);
+
         const img = document.createElement('img');
         img.setAttribute("src", picture)
         const h2 = document.createElement('h2');
@@ -33,31 +33,3 @@ function photographerFactory(data) {
     }
     return { name, picture, city, country, tagline, price, getUserCardDOM }
 }
-
-// function photographerFactory(data) {
-//     const { name, portrait, city, country, tagline, price } = data;
-
-//     const picture = `assets/photographers/${portrait}`;
-
-//     function getUserCardDOM() {
-//         // const link = document.createElement('a');
-//         // const img = document.createElement('img');
-//         // link.setAttribute("src", 'photographer.html')
-//         // const h2 = document.createElement('h2');
-//         // h2.textContent = name;
-//         // const h3 = document.createElement('h3');
-//         // h3.textContent = city + ',  ' + country;
-//         // const h4 = document.createElement('h4');
-//         // h4.textContent = tagline;
-//         // const p = document.createElement('p');
-//         // p.textContent = price + ' €/jour';
-//         // link.appendChild(img);
-//         // link.appendChild(h2);
-//         // link.appendChild(h3);
-//         // link.appendChild(h4)
-//         // link.appendChild(p);
-
-//         // return (link);
-//     }
-//     return { name, picture, city, country, tagline, price, getUserCardDOM }
-// }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,6 +1,36 @@
-//Mettre le code JavaScript lié à la page photographer.html
+import getPhotographers from '/data/api.js'
+// import displayData from '/scripts/'
+
+// //Mettre le code JavaScript lié à la page photographer.html
+
+const id = getId()
+
+async function init() {
+    // Récupère les datas des photographes
+    const { photographers, media } = await getPhotographers();
+    const photographer = photographers.find(a => a.id === id);
+    const medias = media.filter(a => a.photographerId === id);
+    console.log(photographer, medias)
 
 
+    // displayData(photographer, medias);
+
+};
+
+init()
+
+// Ici nous récupérons l'ID du photographe grace au query string
+
+function getId() {
+    const params = new Proxy(new URLSearchParams(window.location.search), {
+        get: (searchParams, prop) => searchParams.get(prop),
+    });
+    console.log(params.id)
+    return Number(params.id);
+
+}
+
+// console.log(params.id)
 
 
 
