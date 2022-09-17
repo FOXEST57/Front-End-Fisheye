b7e90afecda479a894455fac244a2ce349916b97
Foxest57 <108059438+FOXEST57@users.noreply.github.com>
2022-09-17 16:32:38 +0200

    sauvegarde

--- scripts/factories/image.js
+++ scripts/factories/image.js
@@ -20,3 +20,36 @@
 // }
 
 // export default imageFactory;
+
+
+
+function imageFactory(data) {
+    const { name, portrait, city, country, tagline, price, id } = data;
+
+    const picture = `assets/photographers/${portrait}`;
+
+    function getUserCardDOM() {
+        const article = document.createElement('a');
+        article.setAttribute('href', 'photographer.html?id=' + id);
+
+        const img = document.createElement('img');
+        img.setAttribute("src", picture)
+        const h2 = document.createElement('h2');
+        h2.textContent = name;
+
+        const h3 = document.createElement('h3');
+        h3.textContent = city + ',  ' + country;
+        const h4 = document.createElement('h4');
+        h4.textContent = tagline;
+        const p = document.createElement('p');
+        p.textContent = price + ' €/jour';
+        article.appendChild(img);
+        article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(h4)
+        article.appendChild(p);
+
+        return (article);
+    }
+    return { name, picture, city, country, tagline, price, getUserCardDOM }
+}
--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -16,18 +16,20 @@ function photographerFactory(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement('h2');
         h2.textContent = name;
-
-        const h3 = document.createElement('h3');
-        h3.textContent = city + ',  ' + country;
-        const h4 = document.createElement('h4');
-        h4.textContent = tagline;
-        const p = document.createElement('p');
-        p.textContent = price + ' €/jour';
+        const ville = document.createElement('p');
+        ville.className = "city"
+        ville.textContent = city + ',  ' + country;
+        const descriptif = document.createElement('p');
+        descriptif.className = "descriptif"
+        descriptif.textContent = tagline;
+        const prix = document.createElement('p');
+        prix.className = "price"
+        prix.textContent = price + ' €/jour';
         article.appendChild(img);
         article.appendChild(h2);
-        article.appendChild(h3);
-        article.appendChild(h4)
-        article.appendChild(p);
+        article.appendChild(ville);
+        article.appendChild(descriptif)
+        article.appendChild(prix);
 
         return (article);
     }
